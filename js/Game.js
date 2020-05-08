9e15f1c471cfc8834d0c5fcc2bc1a7b2e86a9158
nandita <nanditanair612@gmail.com>
2020-05-08 16:03:13 +0530

    Database

--- js/Game.js
+++ js/Game.js
@@ -23,4 +23,34 @@ class Game {
       form.display();
     }
   }
+  play(){
+    form.hide();
+    textSize(30);
+    text("game Start! ",120,100);
+    Player.getPlayerInfo();
+    if(allPlayers!== undefined){
+      var display_Position =130 ;
+      for(var plr in allPlayers){
+        if(plr==="player"+player.index){
+          fill (" red")
+                  }
+        else{
+          fill("black")
+        }
+      
+      display_Position += 20;
+      textSize(15);
+      text(allPlayers[plr].name+":"+ allPlayers[plr].distance);
+
+
+
+
+
+    }
+  }
+  if(keyIsDown(UP_ARROW)&& player.index!=null){
+player.distance+=50;
+player.updateCount();
+  }  
+  }
 }
